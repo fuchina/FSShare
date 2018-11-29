5772c43abd42c910d392be3f7dd22172c2ac38ba
fudon <fuart@qq.com>
2018-11-29 23:17:15 +0800

    配置工程

---
+++ FSShareSample/FSShareSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FSShareSample
+//
+//  Created by FudonFuchina on 2018/11/29.
+//  Copyright © 2018年 FudonFuchina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FSShareSample/FSShareSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FSShareSample
+//
+//  Created by FudonFuchina on 2018/11/29.
+//  Copyright © 2018年 FudonFuchina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
